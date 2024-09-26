6b72ee5502b7064c834b0fb7ccccac48c7216869
Izushi <shoeshinesamurai@gmail.com>
2024-09-26 12:29:06 -0700

    ex02.js is done

--- ex02.js
+++ ex02.js
@@ -5,6 +5,10 @@
 
 function subtract(...numbers) {
   // Your code here
+  const subtractedNum = numbers.reduce((subtracted, currentNum) => {
+    return subtracted - currentNum
+  });
+  return subtractedNum;
 }
 
 console.log(subtract(10, 2, 3)); // Example usage
